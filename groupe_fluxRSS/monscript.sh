8a9d66042bdcf7a1636b47aaa57cf92cfa67bfa9
elodie <elodie_boy@hotmail.fr>
2014-01-09 14:00:22 +0100

    modification des dossiers

--- groupe_fluxRSS/monscript.sh
+++ groupe_fluxRSS/monscript.sh
@@ -1,8 +1,9 @@
 cat monscript.sh
 #!/bin/bash
 
-wget -q -O - http://rss.leparisien.fr/leparisien/rss/une.xml | sed -n 's#.*<title>\(.*\)</title>.*#\1#p'
-wget -q -O - http://rss.leparisien.fr/leparisien/rss/une.xml | sed -n 's#.*<description>\(.*\)</description>.*#\1#p'
+
+wget -q -O - http://www.sport.fr/RSS/sport1.xml | sed -n 's#.*<title>\(.*\)</title>.*#\1#p'
+wget -q -O - http://www.sport.fr/RSS/sport1.xml | sed -n 's#.*<description>\(.*\)</description>.*#\1#p'
 
 sh monscript.sh > contenu.txt
 
--- groupe_fluxRSS/scriptMail.sh
+++ groupe_fluxRSS/scriptMail.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
-SUBJECT=”le sujet de votre Email”
-EMAIL=”elodieboy91@gmail.com”
-echo $EMAILMESSAGE | /bin/mail -s “$SUBJECT” “$EMAIL”
+#!/bin/bash
+# Authors : groupe_fluxRSS (Ayako Dumont, Guillaume Prou, Elodie Boy)
+
+echo "test" | mail -s "test" elodieboy91@gmail.com
